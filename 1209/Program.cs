669833b98f982ed508e4f73cdb8cde9620adcc1d
Nandor206 <nandornotfound@gmail.com>
2024-12-09 13:12:39 +0100

    1209 folder created

---
+++ 1209/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+class Utvonal {
+    const int utHossz = 100;
+    static int[] magas = new int[utHossz];
+
+    static void General() {
+        Random rnd = new Random(); // new();
+        magas[0] = rnd.Next(10,30);
+        for (int i = 1; i < magas.Length; i++)
+        {
+            magas[i] = magas[i-1] + rnd.Next(11) - 5; // -5, -4, -3, ..., 3, 4, 5
+        }
+        for (int i = 0; i < magas.Length; i++)
+        {
+            if(magas[i]<0) magas[i]=0; // a tenger alattiból lesz tenger
+        }
+    }
+
+    static void Kirajzol() {
+        int maxMagas = magas.Max();
+        for (int magi = maxMagas+2; magi >= 0; magi--)
+        {
+           System.Console.Write($"{magi,3} ");
+           for (int xi = 0; xi < magas.Length; xi++)
+           {
+                if( magi>magas[xi] ) Console.Write(' ');
+                else Console.Write('\u2588');
+           }
+           System.Console.WriteLine( " " + magi );
+        }
+
+
+    }
+
+    static void Feliratok() {
+        System.Console.Write("    ");
+        for (int i = 0; i < magas.Length; i++)
+        {
+            if( i % 10 == 0 ) Console.Write( i/10 ); // kétjegyűekig jó
+            else Console.Write(' ');
+        }
+        System.Console.WriteLine();
+        System.Console.Write("    ");
+        for (int i = 0; i < magas.Length; i++)
+        {
+            Console.Write( i % 10 );
+        }
+    }
+
+    static void SzarazAtlag() {
+        int osszeg = 0;
+        int darab = 0;
+        for (var i = 0; i < magas.Length; i++)
+        {
+            if (magas[i]>0)
+            {
+                osszeg += magas[i];
+                darab++;
+            }
+        }
+        double atlag = osszeg / (double)darab;
+        System.Console.WriteLine($"Szárazföldek átlaga: {atlag:f1}");
+    }
+    
+    static void Main() {
+        General();
+        Feliratok();
+        Kirajzol();
+        Feliratok();
+    }
+} // end class
