3a5a8a9f12587d059a98104a8e33c5a223441fc9
Nandor206 <nandornotfound@gmail.com>
2024-12-09 14:31:50 +0100

    Build test + 2 feladat is done

--- 1209/Sorozatok.cs
+++ 1209/Sorozatok.cs
@@ -22,9 +22,38 @@ class Sorozatok {
         }
         System.Console.WriteLine();
     }
+
+    static void Feladat1() {
+        Beolvas();
+        var szorzat = 1.0;
+        for (var i = 0; i < tomb.Length; i++)
+        {
+            if (!(tomb[i]<0))
+            {
+                szorzat = szorzat*tomb[i];
+            }
+        }
+        System.Console.WriteLine("Eredméyn: "+szorzat);
+    }
+
+    static void Feladat2() {
+        Beolvas();
+        double elotte = tomb[0];
+        System.Console.WriteLine("Ezek a számok nagyobbak, mint szomszédjaik: ");
+        for (var i = 1; i < tomb.Length-1; i++)
+        {
+            if (tomb[i]>elotte && tomb[i]>tomb[i+1])
+            {
+                System.Console.Write(tomb[i]);
+            }
+            elotte = tomb[i];
+        }
+    }
     public static void Main()
     {
-        Beolvas();
-        Kiir();
+        //Beolvas();
+        //Kiir();
+        //Feladat1();
+        //Feladat2();
     }
 }
