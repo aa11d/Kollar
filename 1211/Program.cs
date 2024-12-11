1297d022cbec40fd057ba32ff612cafd3700a28d
Nandor206 <nandornotfound@gmail.com>
2024-12-11 09:15:18 +0100

    1211 created with base code

---
+++ 1211/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program()
+{
+
+    static void Main()
+    {
+        Uno();
+    }
+    static void Uno()
+    {
+        System.Console.Write("Szöveg: ");
+        var Sor = Console.ReadLine();
+        string[] darab = Sor.Split(' ');
+        int[] szavaz = new int[darab.Length];
+        for (int i = 0; i < szavaz.Length; i++)
+        {
+            szavaz[i] = int.Parse(darab[i]);
+        }
+        
+    }
+
+}
