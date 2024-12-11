c1c8b566b7da302a6a5c43470dfa10d97d789d89
Nandor206 <nandornotfound@gmail.com>
2024-12-11 10:11:44 +0100

    Not working, yet

--- 1211/Program.cs
+++ 1211/Program.cs
@@ -2,12 +2,9 @@
 
 class Program()
 {
-
+    static int[] szavaz;
+    static int[] kapott = new int[11];
     static void Main()
-    {
-        Uno();
-    }
-    static void Uno()
     {
         System.Console.Write("Szöveg: ");
         var Sor = Console.ReadLine();
@@ -16,8 +13,25 @@ class Program()
         for (int i = 0; i < szavaz.Length; i++)
         {
             szavaz[i] = int.Parse(darab[i]);
+            System.Console.Write(szavaz[i]+" ");
         }
-        
+        Uno();
+    }
+
+    static void Uno()
+    {
+        for (int i = 1; i < kapott.Length; i++)
+        {
+            for (int j = 0; j < szavaz.Length; j++)
+            {
+                if (szavaz[j] == i)
+                {
+                    kapott[i]++;
+                }
+            }
+        }
+        System.Console.WriteLine("0 1 2 3 4 5 6 7 8 9");
+        System.Console.WriteLine(kapott);
     }
 
 }
--- 1211/obj/Debug/net8.0/1211.AssemblyInfo.cs
+++ 1211/obj/Debug/net8.0/1211.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1211")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1297d022cbec40fd057ba32ff612cafd3700a28d")]
 [assembly: System.Reflection.AssemblyProductAttribute("1211")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1211")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
