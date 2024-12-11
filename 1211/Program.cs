2d88b2798849a7d9e82ada538569fcfa3d19e384
Nandor206 <nandornotfound@gmail.com>
2024-12-11 11:09:46 +0100

    Task 2 done

--- 1211/Program.cs
+++ 1211/Program.cs
@@ -1,26 +1,65 @@
 using System;
 
-class Program()
+class Program
 {
     public static void Main()
     {
-        uno();
+        tres();
     }
 
     static void uno()
     {
         System.Console.Write("Pont: ");
         var Sor = Console.ReadLine();
-        int[] pontok = Int32.Parse(Sor.Split(' '));
-        int darab = 0;
-        for (int i = 1; i < pontok.Length; i++)
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
         {
-            if (pontok[i]>pontok[i-1] && pontok[i]>pontok[i+1])
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int darab = 0;
+        for (int i = 1; i < pontok.Length-1; i++)
+        {
+            if ((pontok[i]>pontok[i-1]) && (pontok[i]>pontok[i+1]))
             {
                 darab++;
             }
         }
         System.Console.WriteLine("Válasz: "+darab);
+    }
+
+    static void dos()
+    {
+        System.Console.Write("KM: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int idoszak = 0;
+        if (pontok[0]>10)
+            {
+                idoszak++;
+            }
+        for (int i = 1; i < pontok.Length; i++)
+        {
+            if (pontok[i-1]>10)
+            {
+                continue;
+            }
+            if (pontok[i]>10)
+            {
+                idoszak++;
+            }
+            
+        }
+        System.Console.WriteLine("DB: "+idoszak);
     }
 
 }
