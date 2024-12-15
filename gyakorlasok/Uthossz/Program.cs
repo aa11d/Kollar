3f96ebd3f4bc9c9213f8fd14beeaf95ead571ae2
Nandor206 <nandornotfound@gmail.com>
2024-12-15 10:49:15 +0100

    gyakorlasok has been made

---
+++ gyakorlasok/Uthossz/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        
+        // Your code here
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
