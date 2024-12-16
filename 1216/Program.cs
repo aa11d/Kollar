5ddd4628a8bd1cd3206b39d7cd657663e5c0d978
Nandor206 <nandornotfound@gmail.com>
2024-12-16 13:24:38 +0100

    Essential stuff

---
+++ 1216/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+class Megoldasok
+{
+    static void Main(string[] args)
+    {
+        Feladat3();
+    }
+
+    static void Feladat1()
+    {
+        System.Console.Write("KM: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int ossz = 0;
+        for (var i = 0; i < pontok.Length; i++)
+        {
+            ossz += pontok[i];
+        }
+
+        System.Console.WriteLine("Út: "+ossz);
+    }
+
+    static void Feladat2()
+    {
+        System.Console.Write("Pont: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int ossz = 0;
+        for (var i = 0; i < pontok.Length; i++)
+        {
+            if (pontok[i] < 5)
+            {
+                ossz+= pontok[i];
+            }
+        }
+        System.Console.WriteLine("Válasz: "+ossz);
+    }
+
+    static void Feladat3()
+    {
+        System.Console.Write("Befutott: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int db = 0;
+        for (var i = 0; i < pontok.Length; i++)
+        {
+            if (pontok[i] < 4 && pontok[i] >= 1)
+            {
+                db++;
+            }
+        }
+        System.Console.WriteLine("Válasz: "+db);
+    }
+
+    static void Feladat4()
+    {
+        System.Console.Write("Befutott: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int elso = -1;
+        for (var i = 0; i < pontok.Length; i++)
+        {
+            if (pontok[i] > 9)
+            {
+                elso = i;
+                break;
+            }
+        }
+        if (elso != -1)
+        {
+            System.Console.WriteLine("Válasz: "+elso);
+        }
+        else
+        {
+            System.Console.WriteLine("Válasz: Nem volt");
+        }
+    }
+}
