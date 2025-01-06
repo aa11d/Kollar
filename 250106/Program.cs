2cf0951a23ac413e39eba81aae2fc8dc02ff82f2
Nandor206 <nandornotfound@gmail.com>
2025-01-06 14:45:24 +0100

    Done until feladat9

--- 250106/Program.cs
+++ 250106/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Formats.Tar;
+using System.IO.Compression;
 class Megoldasok {
     static void Main(string[] args)
     {
@@ -111,29 +111,170 @@ class Megoldasok {
             pontok[i] = int.Parse(pontok_s[i]);
         }
 
-        int lk = 0;
+        int lk = 1000000000;
         int ut = 0;
         bool tf = true;
         for (var i = 0; i < hossz-2; i++)
         {
+            tf = true;
             for (var l = 0; l < 3; l++)
             {
-                if (pontok[l] != 0)
+                if (pontok[i+l] != 0)
                 {
-                    ut += pontok[i];
+                    ut += pontok[i+l];
                 }
-                if (pontok[l] == 0)
+                if (pontok[l+i] == 0)
                 {
-                    break;
                     tf = false;
+                    break;
                 }
-                ut = 0;
             }
             if (tf)
             {
-                lk = ut;
+                if(ut < lk ) 
+                    lk = ut;
             }
+            ut = 0;
         }
         System.Console.WriteLine(lk);
+    }
+
+    static void Feladat6()
+    {
+        System.Console.Write("Pontok: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int db = 0;
+        for (var i = 0; i < hossz; i++)
+        {
+            if (pontok[i] >= 5 && pontok[i] <= 10)
+            {
+                db++;
+            }
+        }
+        System.Console.WriteLine($"Válasz: {db}");
+    }
+
+    static void Feladat7()
+    {
+        System.Console.Write("Befutott: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int db = 1;
+        int num = 0;
+        for (var i = 0; i < hossz; i++)
+        {
+            if (num < pontok[i])
+            {
+                num = pontok[i];
+                db++;
+            }
+        }
+        System.Console.Write("Válasz: ");
+        for (var i = 0; i < hossz; i++)
+        {
+            if (num == pontok[i])
+            {
+                System.Console.Write(i+1+"");
+            }
+        }
+        
+    }
+
+    static void Feladat8()
+    {
+        System.Console.Write("Befutott: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int num = 0;
+        for (var i = 0; i < hossz; i++)
+        {
+            if (pontok[i] > 1)
+            {
+                num += pontok[i];
+            }
+        }
+        if (num != 0)
+        {
+            System.Console.WriteLine("Válasz: "+num);
+        }
+        else{
+            System.Console.Write("Válasz: Nem volt");
+        }
+    }
+
+static void Feladat9()
+    {
+        System.Console.Write("Befutott: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int num = pontok.Max();
+        int db = -1;
+        for (var i = 0; i < hossz; i++)
+        {
+            if (pontok[i] == num)
+            {
+                db = i+1;
+            }
+            if (db > -1)
+            {
+                break;
+            }
+        }
+        System.Console.Write("Válasz: "+db);
+    }
+
+    static void Feladat10()
+    {
+        System.Console.Write("Befutott: ");
+        var Sor = Console.ReadLine();
+        string[] pontok_s = Sor.Split(' ');
+        int hossz = pontok_s.Length;
+        int[] pontok = new int[hossz];
+        for (int i = 0; i < pontok.Length; i++)
+        {
+            pontok[i] = int.Parse(pontok_s[i]);
+        }
+
+        int perc = 0;
+        int db = 0;
+        while (pontok[perc-1] < 20)
+        {
+            db += pontok[perc];
+            perc++;
+        }
+        if (perc-1 == hossz)
+        {
+            perc = hossz;
+        }
+        System.Console.WriteLine("Válasz: "+perc);
     }
 }
--- 250106/obj/Debug/net8.0/250106.AssemblyInfo.cs
+++ 250106/obj/Debug/net8.0/250106.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("250106")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68182ba9bb25d7216419286a409ecebccb37c5a5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3a2263c7196a9fa6a84c9034369f57f6020b5c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("250106")]
 [assembly: System.Reflection.AssemblyTitleAttribute("250106")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
