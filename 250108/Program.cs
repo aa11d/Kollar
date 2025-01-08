1cba97b68378c12341b4c2c8bbfa10311eef2ee5
Nandor206 <nandornotfound@gmail.com>
2025-01-08 11:04:53 +0100

    Almost done

--- 250108/Program.cs
+++ 250108/Program.cs
@@ -2,9 +2,12 @@
 
 class TobbDimTomb {
     static void Main() {
-        KetDimTomb();
+        //KetDimTomb();
+        Amoba();
     }
     static Random rnd = new Random();
+    static char[,] palya = new char[20, 20];
+    static bool elozoX = false;
 
     static void KetDimTomb() {
         const int sorSz = 6;
@@ -26,6 +29,77 @@ class TobbDimTomb {
                 System.Console.Write(kart[sor, oszlop]);
             }
             System.Console.WriteLine();
+        }
+    }
+
+    static void Amoba() {
+        for (int sor = 0; sor < palya.GetLength(0); sor++)
+        {
+            for (int oszlop = 0; oszlop < palya.GetLength(1); oszlop++)
+            {
+                palya[sor, oszlop] =' ';
+            }
+        }
+
+        while (!VanOtos()) {
+            PalyaKi();
+            Lep();
+            if (!VanOtos()) {
+
+            }
+        }
+    }
+
+    static bool VanOtos() {
+        return false;
+    }
+
+    static void PalyaKi() {
+        System.Console.Write(' ');
+        for (int oszlop = 0; oszlop < palya.GetLength(1); oszlop++)
+        {
+            System.Console.Write(oszlop % 10);
+        }
+        System.Console.WriteLine();
+        for (int sor = 0; sor < palya.GetLength(0); sor++)
+        {
+            System.Console.Write(sor % 10);
+            for (int oszlop = 0; oszlop < palya.GetLength(1); oszlop++)
+            {
+               System.Console.Write(palya[sor, oszlop]); 
+            }
+            System.Console.WriteLine();
+        }
+    }
+    static void Lep() {
+        if (!elozoX)
+        {
+        System.Console.Write("X Jön, x: ");
+        int inputX = int.Parse(Console.ReadLine());
+        System.Console.Write("y: ");
+        int inputY = int.Parse(Console.ReadLine());
+            if (palya[inputX, inputY] == ' ')
+            {
+                palya[inputX, inputY] = 'X';
+                elozoX = true;
+            }
+            else {
+                System.Console.WriteLine("Foglalt terület! Próbálja újra.  (x: " + inputX + ", y: " + inputY + ")");
+            }
+        }
+        else {
+        System.Console.Write("O Jön, x: ");
+        int inputX = int.Parse(Console.ReadLine());
+        System.Console.Write("y: ");
+        int inputY = int.Parse(Console.ReadLine());
+        if (palya[inputX, inputY] == ' ')
+        {
+            palya[inputX, inputY] = 'O';
+            elozoX = false;
+        }
+        else {
+            System.Console.WriteLine("Foglalt terület! Próbálja újra.  (x: " + inputX + ", y: " + inputY + ")");
+        }
         }
     }
 }
