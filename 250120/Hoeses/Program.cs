8326a424d96346964a98d2b83537410bd9efc847
Nandor206 <nandornotfound@gmail.com>
2025-01-20 14:46:36 +0100

    Basic kiírás

--- 250120/Hoeses/Program.cs
+++ 250120/Hoeses/Program.cs
@@ -5,8 +5,36 @@ class Hoeses
 
     const int N = 30;
     const int M = 50;
+    
+    static string[,] map = new string[N, M];
     static void Main(string[] args)
     {
-        
+        Gen();
+        Draw();
+    }
+
+    static void Gen(){
+        for (int y = 0; y < map.GetLength(0); y++)
+        {
+            for (int x = 0; x < map.GetLength(1); x++)
+            {
+                map[x, y] = "*";
+                if (y == N-1)
+                {
+                    map[x, y] = "H";
+                }
+            }
+        }
+    }
+
+    static void Draw(){
+        for (int y = 0; y < map.GetLength(1); y++)
+        {
+            for (int x = 0; x < map.GetLength(0); x++)
+            {
+                Console.Write(map[x, y]);
+            }
+            Console.WriteLine();
+        }
     }
 }
