58e8689fd8fe57dbdcf5334632eb8823ddf253bc
Nandor206 <nandornotfound@gmail.com>
2025-01-22 11:16:49 +0100

    Whyyyyyyyyyyyyyyy???????

--- 250120-22/Hoeses/obj/Debug/net9.0/Hoeses.AssemblyInfo.cs
+++ 250120-22/Hoeses/obj/Debug/net9.0/Hoeses.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hoeses")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd2fc361899cab6e8d9a6f866071d443b26ff9d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+141e9d3a5d1cf17dc622416ee37c3ec882357aa7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hoeses")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hoeses")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
